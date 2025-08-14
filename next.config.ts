1e22b8412477ae81801931cad87ba98bdc5c5fa1
Rohit kumar <rohitraj051@gmail.com>
2025-08-14 09:37:31 +0530

    Build for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   images: {
     domains: ['cdn.dummyjson.com'],
-  
+    unoptimized: true
   },
+  basePath: process.env.NODE_ENV === "production" ? "/patna" : "",
 };
 
 module.exports = nextConfig;
